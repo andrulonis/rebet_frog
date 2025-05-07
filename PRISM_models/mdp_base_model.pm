0e7ca80d386afbf1e51cd8de03f860f7a0678b26
Daanvduin <daanvduin02@gmail.com>
2025-05-07 16:34:35 +0200

    use example mdp model

--- PRISM_models/mdp_base_model.pm
+++ PRISM_models/mdp_base_model.pm
@@ -1,29 +1,20 @@
 mdp
 
-module camera
+module main
     c: [0..1];
-    r: [1..3];
-    [goto0] true -> (c'=0) & (r'=1); 
-    [goto1] true -> (c'=0) & (r'=2); 
-    [goto2] true -> (c'=0) & (r'=3);
-    [goto3] true -> (c'=1) & (r'=1);
-    [goto4] true -> (c'=1) & (r'=2);
-    [goto5] true -> (c'=1) & (r'=3);
+    r: [1..2];
+
+    total_pics: [0..10];
+
+    [config0] total_pics < 10 -> (c'=0) & (r'=1) & (total_pics'=total_pics+1);
+    [config1] total_pics < 9 -> (c'=0) & (r'=2) & (total_pics'=total_pics+2);
+    [config2] total_pics < 10 -> (c'=1) & (r'=1) & (total_pics'=total_pics+1);
+    [config3] total_pics < 9 -> (c'=1) & (r'=2) & (total_pics'=total_pics+2);
 endmodule
 
-label "goal" = c=1 & r!=1;
+label "goal" = total_pics = 10; 
 
-rewards "energy"
-    [goto0]c=1:1;
-    [goto0]c=0:0.1;
-    [goto1]c=1:2;
-    [goto1]c=0:0.2;
-    [goto2]c=1:1;
-    [goto2]c=0:0.1;
-    [goto3]c=0:3;
-    [goto3]c=1:0.3;
-    [goto4]c=0:1;
-    [goto4]c=1:0.1;
-    [goto5]c=0:4;
-    [goto5]c=1:0.4;
+rewards
+    c=0 : r;
+    c=1 : 2*r;
 endrewards
