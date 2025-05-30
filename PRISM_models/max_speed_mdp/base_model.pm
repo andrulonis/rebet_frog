e419e8580aae09ccf5c9563c8004f9320730e1b2
andrulonis <48800417+andrulonis@users.noreply.github.com>
2025-05-30 16:17:50 +0200

    Adjust model for speed
    Co-authored-by: Daanvduin <Daanvduin@users.noreply.github.com>

--- PRISM_models/max_speed_mdp/base_model.pm
+++ PRISM_models/max_speed_mdp/base_model.pm
@@ -5,14 +5,14 @@ const c1_speed = 26; // cm/s
 
 const double unsafe_threshold = 0.1;
 
-formula p_unsafe_short_term = current_safety < unsafe_threshold ? 1 : 1 - (current_safety - unsafe_threshold); // find magic value for the last 1 because usually the safety will be far below 1
-formula p_unsafe_long_term = min(1 - (mean_safety - unsafe_threshold), 1 - (unsafe_threshold - mean_safety));
+formula p_unsafe_short_term = current_safety < unsafe_threshold ? 1 : (current_safety > 1.5*unsafe_threshold ? 0 : (1.5*unsafe_threshold - current_safety) / unsafe_threshold);
+formula p_unsafe_long_term =  mean_safety < 0.5*unsafe_threshold ? 1 : (mean_safety > 1.5*unsafe_threshold ? 0 : (1.5*unsafe_threshold - mean_safety) / unsafe_threshold);
 
 module main
     distance_to_pose : [0..distance_to_pose_init] init distance_to_pose_init; // assumed to be in cm
     is_fast : [0..1] init is_fast_init;
     is_unsafe : [0..1] init is_unsafe_init;
-    short_term : [0..1] init short_term_init; // boolean, we want to use a different probability for this adaptation than for future ones.
+    short_term : [0..1] init short_term_init; // boolean, we want to use a different probability for this adaptation than for future ones
 
     [config0] short_term = 1 ->  p_unsafe_short_term : (distance_to_pose'=max(distance_to_pose-c0_speed,0)) & (short_term'=0) & (is_unsafe'=1) & (is_fast'=0) + 
                                 (1-p_unsafe_short_term) : (distance_to_pose'=max(distance_to_pose-c0_speed,0)) & (short_term'=0) & (is_unsafe'=0) & (is_fast'=0);
@@ -25,6 +25,6 @@ module main
 endmodule
 
 rewards
-    is_unsafe = 1 & is_fast = 1: 1;
-    is_unsafe = 0 & is_fast = 0: 1;
+    is_unsafe = 1 & is_fast = 1: 0.5;
+    true: 1;
 endrewards
