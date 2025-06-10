0a4aa2950070799628997f8e6116b37b37f99b53
andrulonis <48800417+andrulonis@users.noreply.github.com>
2025-06-10 15:24:05 +0200

    New trees
    Co-authored-by: Daanvduin <Daanvduin@users.noreply.github.com>

--- launch/spawn_tb3.launch.py
+++ launch/spawn_tb3.launch.py
@@ -50,14 +50,14 @@ def launch_setup(context, *args, **kwargs):
     robot_position = {'pose':(0.0,0.0,0.0), 'size': (1, 1, 1)}
     
     for _ in range(num_entities):
-        entity_x_random = np.random.uniform(low=-1.2, high=1.8)
+        entity_x_random = np.random.uniform(low=-1.4, high=1.8)
         entity_y_random = np.random.uniform(low=-1.8, high=1.8)
 
         while(is_point_inside_entities(entity_x_random,entity_y_random, entities + [robot_position])):
-            entity_x_random = np.random.uniform(low=-1.2, high=2)
+            entity_x_random = np.random.uniform(low=-1.4, high=1.8)
             entity_y_random = np.random.uniform(low=-1.8, high=1.8)
 
-        entity = {'pose': (entity_x_random, entity_y_random, -0.14), 'size': (3.0, 2, 1.2)}
+        entity = {'pose': (entity_x_random, entity_y_random, -0.14), 'size': (2.0, 2.0, 1.2)}
 
         entities.append(entity)
 
